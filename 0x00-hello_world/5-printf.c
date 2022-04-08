a110d6e5087b076ca2195c08e4a0025f68296a26
uche <uchegodswill480@gmail.com>
2022-04-08 17:13:46 +0100

    printing with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - defines the entry point of our code
+ * Return: 0 if exited successfully,and any value if otherwise.
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

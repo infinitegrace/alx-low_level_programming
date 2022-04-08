8f453a0f0fae435049acb5a02291f06faf79842e
uche <uchegodswill480@gmail.com>
2022-04-08 15:40:32 +0100

    printing with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,9 @@
  * Return: 0 if exited properly,non-zero if otherwise
  */
 
-int main (void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }

97317ce8e51eae34975857743797134d37d22701
uche <uchegodswill480@gmail.com>
2022-04-08 17:16:21 +0100

    space btween function and open parenthesis removed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main - defines the entry point of our code
  * Return: 0 if exited successfully,and any value if otherwise.
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);

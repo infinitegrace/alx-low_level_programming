1fa2d92edbe99b43dfb3a91bab515802066dbccc
uche <uchegodswill480@gmail.com>
2022-04-10 22:29:09 +0100

    upper letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - entry point of code
+ * Return: 0 if successful any value if otherwise
+ */
+int main(void)
+{
+int x;
+
+for (x = 'A'; x <= 'Z'; x++)
+/* jumps from 'A'(65) to 'B'(66) to ... in order*/
+{
+        putchar(tolower(x));
+/*the return value is returned as a parameter */
+}
+for (x = 'A'; x <= 'Z'; x++)
+{
+	putchar(toupper(x));
+}
+putchar('\n');
+return (0);
+}

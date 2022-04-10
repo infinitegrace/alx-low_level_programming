5f8a1f0da02c6e5e66c1f56e392b83d1ddc7b569
uche <uchegodswill480@gmail.com>
2022-04-10 21:25:00 +0100

    print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,16 +4,15 @@
  * main - entry point of code
  * Return: 0 if successful any value if otherwise
  */
-int main()
+int main(void)
 {
 	int x;
 
-    for(x = 'A'; x <= 'Z'; x++) 
+	for (x = 'A'; x <= 'Z'; x++) 
   /* jumps from 'A'(65) to 'B'(66) to ... in order*/
-    {
+	{
         putchar(tolower(x));
-	putchar(' ');
   /*the return value is returned as a parameter */
-    }
+	}
     return (0);
 }

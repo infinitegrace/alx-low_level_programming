76ea70868cd2f8c4375d0d3ee38e1ab81b5121f7
uche <uchegodswill480@gmail.com>
2022-04-11 10:22:38 +0100

    low character

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 alwas if success
+ */
+int main(void)
+{
+int x;
+char letter;
+
+for (x = 0; x < 10; x++)
+{
+	putchar((x % 16 ) + '0');
+}
+for (letter = 'a'; letter <= 'f'; letter++)
+{
+	putchar(letter);
+}
+putchar('\n');
+return (0);
+}
+

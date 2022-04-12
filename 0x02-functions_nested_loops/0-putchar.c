e023df00eac7ae0118e6c6812873a3c973c1675d
uche <uchegodswill480@gmail.com>
2022-04-12 16:17:37 +0100

    nested

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and error is set appropriately.
  */
-int _putchar(char c)
+int _putchar(int c)
 {
-	return (write(1, &c, 1));
+	return (c);
 }

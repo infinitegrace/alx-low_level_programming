3d7e727afb169f7857f2370f310f06ccc5dae5a9
uche <uchegodswill480@gmail.com>
2022-04-12 16:32:06 +0100

    nested

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,10 +5,10 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and error is set appropriately.
+ * Return: On success 0.
+ * On error, 1 is returned, and error is set appropriately.
  */
 int _putchar(char c)
 {
-	return (c);
+	return (write(0, &c, 1));
 }

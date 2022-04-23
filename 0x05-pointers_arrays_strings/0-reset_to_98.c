f926109e1ca241460eec161842834e26b931896d
Uche <uchegodswill480@gmail.com>
2022-04-23 16:43:10 -0700

    reseting a variable frm a pointer address

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - resets the variable
+ *our pointer points to.
+ *@n: function parameter.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+A pointer is a verable having as a values the addredress of another variable which it is pointing to.

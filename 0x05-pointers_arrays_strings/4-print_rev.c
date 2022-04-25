96560a1410239756934da8432a842be3d9c276ab
Uche <uchegodswill480@gmail.com>
2022-04-25 15:28:59 +0000

    display in reverse pattern

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_rev - printing string in reverse
+ *@s:-function parameter
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int maxl;
+
+	for (maxl = 0; s[maxl] != '\0'; maxl++)
+	{
+	}
+
+	for (i = maxl; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+	_putchar('\n');
+}
